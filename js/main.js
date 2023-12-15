97049321e4427e6a26145e7e561fa47974adf639
Zudikhin <uzudihin@gmail.com>
2023-12-15 18:33:08 +0300

    please

--- js/main.js
+++ js/main.js
@@ -78,6 +78,49 @@ $(document).ready(function() {
         $(this).parent().find(".project_block_item_img_slider").slick('slickNext');
     });
 
+
+    $(".home_cat_block_wrap_list_item").each(function() {
+
+        $(this).find(".home_cat_block_wrap_list_item_img_slider").slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            dots: false
+        });
+      
+    });
+
+    $(".home_cat_block_wrap_list_item_img_prev").click(function() {
+        $(this).parent().find(".home_cat_block_wrap_list_item_img_slider").slick('slickPrev');
+    });
+
+    $(".home_cat_block_wrap_list_item_img_next").click(function(){
+        $(this).parent().find(".home_cat_block_wrap_list_item_img_slider").slick('slickNext');
+    });
+
+
+    $(".choice_block_main_list_item").each(function() {
+
+        $(this).find(".choice_block_main_list_item_img_slider").slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            dots: false
+        });
+      
+    });
+
+    $(".choice_block_main_list_item_img_prev").click(function() {
+        $(this).parent().find(".choice_block_main_list_item_img_slider").slick('slickPrev');
+    });
+
+    $(".choice_block_main_list_item_img_next").click(function(){
+        $(this).parent().find(".choice_block_main_list_item_img_slider").slick('slickNext');
+    });
+
+
     $('.flat_block_left_nav').slick({
         slidesToShow: 3,
         mobileFirst: true,
@@ -123,17 +166,17 @@ $(document).ready(function() {
         $(`#${attr}`).addClass("active");
     });
 
-    $('.similar_block').slick({
-        slidesToScroll: 1,
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        autoplay: true,
-        dots: false,
-        arrows: false,
-        variableWidth: true
-    });
+    // $('.similar_block').slick({
+    //     slidesToScroll: 1,
+    //     dots: true,
+    //     infinite: true,
+    //     speed: 300,
+    //     slidesToShow: 1,
+    //     autoplay: true,
+    //     dots: false,
+    //     arrows: false,
+    //     variableWidth: true
+    // });
 
     $(".contacts_office_nav_btns_item").click(function() {
         $(".contacts_office_nav_btns_item").removeClass("active");
@@ -166,9 +209,154 @@ $(document).ready(function() {
         }
     });
 
+
+    $('.home_cat_block_wrap_search input').on('input',function(e){
+        var valueInput = $(this).val();
+        if(valueInput) {
+            $(".home_cat_block_wrap_search_remove").addClass("active");
+        } else {
+            $(".home_cat_block_wrap_search_remove").removeClass("active");
+        }
+    });
+
+    $(".home_cat_block_wrap_search_remove").click(function() {
+        $('.home_cat_block_wrap_search input').val("");
+        $(this).removeClass("active");
+    });
+
     $(".remove_input").click(function() {
         $(this).parent().find("input").val("");
         $(this).parent().find(".remove_input").css("display", "none");
     });
+
+    $(".stage_block_list_item_top").click(function() {
+        $(this).parent().find(".stage_block_list_item_body").slideToggle();
+        $(this).parent().toggleClass("active");
+    });
+
+    $(".choice_block_sidebar_accordion_item_top").click(function() {
+        $(this).toggleClass("active");
+        $(this).parent().find(".choice_block_sidebar_accordion_item_body").slideToggle();
+    });
+
+
+    $(".also_block").slick({
+        slidesToShow: 1,
+        speed: 200,
+        mobileFirst: true,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 1440,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 1600,
+                settings: {
+                    slidesToShow: 4
+                }
+            }
+        ]
+    });  
+
+    $("#filtBtn").click(function() {
+        $(".back_modal").addClass("active");
+        $(".modal_filter").addClass("active");
+        $("body").addClass("scroll");
+    });
+
+    $(".modal_filter_top_btn").click(function() {
+        $(".back_modal").removeClass("active");
+        $(".modal_filter").removeClass("active");
+        $("body").removeClass("scroll");
+    });
+
+    $(".header_block_left_filter").click(function() {
+        $(".header").toggleClass("drop");
+        $(".back_menu").toggleClass("active");
+        $(".dropdown").toggleClass("active");
+        $("body").toggleClass("scroll");
+    });
+
+    $(".back_menu").click(function() {
+        $(this).removeClass("active");
+        $("body").removeClass("scroll");
+        $(".header").removeClass("drop");
+        $(".dropdown").removeClass("active");
+    });
+
+    $(".back_modal").click(function() {
+        $(this).removeClass("active");
+        $(".modal_filter").removeClass("active");
+        $("body").removeClass("scroll");
+    });
+
+    function setProgress(index) {
+        const calc = ((index + 1) / ($slider.slick('getSlick').slideCount)) * 100;
+      
+        $progressBar
+          .css('background-size', `${calc}% 100%`)
+          .attr('aria-valuenow', calc);      
+    }
+      
+      const $slider = $('.gallery_block');
+      const $progressBar = $('.progress');
+      
+      $slider.on('beforeChange', function(event, slick, currentSlide, nextSlide) {   
+        setProgress(nextSlide);
+      });
+      
+      $slider.slick({
+        slidesToShow: 2,
+        speed: 200,
+        mobileFirst: true,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4
+                }
+            },
+            {
+                breakpoint: 1440,
+                settings: {
+                    slidesToShow: 5
+                }
+            },
+            {
+                breakpoint: 1600,
+                settings: {
+                    slidesToShow: 6
+                }
+            }
+        ]
+      });  
+      
+      setProgress(0);
 
 });
